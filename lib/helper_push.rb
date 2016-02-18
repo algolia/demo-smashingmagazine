43d999111d189d08fb3a7d0d0a26ffd3fd0d85d9
Pixelastic <tim@pixelastic.com>
2016-02-19 00:32:45 +0100

    chore(dead): Remove dead code

--- lib/helper_push.rb
+++ lib/helper_push.rb
@@ -70,32 +70,4 @@ class HelperPush
     end
     nil
   end
-
-  #   index
-  # end
-
-  # # Will push records to the index, by batch
-  # # Records too big or too small will be removed before pushing
-  # def index_json(files, index)
-  #   # Reject files too big or too small
-  #   files.reject! do |file|
-  #     size = File.size(file)
-  #     size == 0 || size > 100_000
-  #   end
-
-  #   # Push records by batch of 1000
-  #   index.clear_index
-  #   files.sort.each_slice(1000) do |batch|
-  #     from = File.basename(batch.first)
-  #     to = File.basename(batch.last)
-  #     puts "Indexing from #{from} to #{to}"
-
-  #     objects = batch.map do |file|
-  #       JSON.parse(File.open(file, 'r').read)
-  #     end
-
-  #     index.add_objects(objects)
-  #   end
-  # end
-
 end
