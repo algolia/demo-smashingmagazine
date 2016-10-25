e7b7d6c55be0a4ecd35e2c9802b675cb80d8d109
Pixelastic <tim@pixelastic.com>
2016-10-25 23:28:17 +0200

    fix(git): Stop ignoring public/

---
+++ public/step3.js
@@ -0,0 +1,152 @@
+(function() {
+  'use strict';
+
+  var globals = typeof window === 'undefined' ? global : window;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = ({}).hasOwnProperty;
+
+  var unalias = function(alias, loaderPath) {
+    var result = aliases[alias] || aliases[alias + '/index.js'];
+    return result || alias;
+  };
+
+  var _reg = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (_reg.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var require = function(name, loaderPath) {
+    if (loaderPath == null) loaderPath = '/';
+    var path = unalias(name, loaderPath);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has.call(cache, dirIndex)) return cache[dirIndex].exports;
+    if (has.call(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '" from ' + '"' + loaderPath + '"');
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          require.register(key, bundle[key]);
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+    }
+  };
+
+  require.list = function() {
+    var result = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  };
+
+  require.brunch = true;
+  require._cache = cache;
+  globals.require = require;
+})();
+require.register("javascripts/step3", function(exports, require, module) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var Search = {
+  init: function init() {
+
+    var search = instantsearch({
+      appId: 'latency',
+      apiKey: 'c80a1f43b1650d1a2225d95d6df0ffab',
+      indexName: 'smashingmagazine'
+    });
+
+    // Search bar
+    search.addWidget(instantsearch.widgets.searchBox({
+      container: '#searchbar',
+      placeholder: 'Search in all Smashing posts',
+      wrapInput: false
+    }));
+
+    var hitTemplate = document.getElementById('templateSearch-hit').innerHTML;
+    var emptyTemplate = 'No results';
+    search.addWidget(instantsearch.widgets.hits({
+      container: '#search-hits',
+      hitsPerPage: 20,
+      cssClasses: {
+        root: 'search-hits'
+      },
+      templates: {
+        empty: emptyTemplate,
+        item: hitTemplate
+      },
+      transformData: {
+        item: function item(data) {
+          // Date in human-readable format
+          data.date = moment.unix(data.publishedDate).format('MMMM Do, YYYY');
+
+          // Number of comments
+          if (data.commentCount > 1) {
+            data.comments = data.commentCount + ' Comments';
+          } else {
+            data.comments = data.commentCout === 0 ? null : '1 Comment';
+          }
+
+          return data;
+        }
+      }
+    }));
+
+    search.start();
+  }
+};
+
+exports.default = Search;
+
+});
+
