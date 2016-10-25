3bba16a3d33291cc8a44d1a8ba2f20df5f4475ce
Pixelastic <tim@pixelastic.com>
2016-10-25 14:59:23 +0200

    Updates

--- step2.js
+++ step2.js
@@ -101,8 +101,8 @@ var Search = {
   init: function init() {
 
     var search = instantsearch({
-      appId: 'KHKP14DMQR',
-      apiKey: '2ad9f79596007d25d292ba994f0554f7',
+      appId: 'latency',
+      apiKey: 'c80a1f43b1650d1a2225d95d6df0ffab',
       indexName: 'smashingmagazine'
     });
 
